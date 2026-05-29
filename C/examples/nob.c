b0f7f70b17b35ddae6ba32cb4b8df839cd8f4450
Arthur Guyot <agp+WSL@gmail.com>
2026-05-29 10:37:08 +0200

    Added vscode tasks

--- C/examples/nob.c
+++ C/examples/nob.c
@@ -1,6 +1,6 @@
 /*
 nob.c
-This file is used to compile example. 
+This file is used to compile example.
 Usage : `gcc nob.c -o nob.elf`
 Then : ./nob.elf     # This will build the examples
 
@@ -12,25 +12,28 @@ See : https://github.com/tsoding/nob.h/blob/main/how_to/015_walk_dirs/nob.c
 #define NOB_EXPERIMENTAL_DELETE_OLD
 #include "nob.h"
 
-#define SRC_FOLDER   "WildPointer/"
+#define UNDEFINED_BEHAVIOR_SRC    "Undefined_Behavior.c"
+#define UNDEFINED_BEHAVIOR_ELF    "Undefined_Behavior.elf"
+#define XMACRO_SRC                "XMacro.c"
+#define XMACRO_ELF                "XMacro_preprocessed.c"
+#define PTR_FUNCTION_APP_SRC      "PTR_Function/app.c"
+#define PTR_FUNCTION_APPFLASH_SRC "PTR_Function/appFlash.c"
 
-#define UNDEFINED_BEHAVIOR_SRC  "Undefined_Behavior.c"
-#define UNDEFINED_BEHAVIOR_ELF  "Undefined_Behavior.elf"
-
-#define XMACRO_SRC  "XMacro.c"
-#define XMACRO_ELF  "XMacro_preprocessed.c"
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
-    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", SRC_FOLDER"segFault.elf", SRC_FOLDER"segFault.c");
-    if (!cmd_run(&cmd)) return 1;
-
 
     cmd_append(&cmd, "cc", UNDEFINED_BEHAVIOR_SRC, "-o", UNDEFINED_BEHAVIOR_ELF);
-    if (!cmd_run(&cmd)) return 1;
+    if (!cmd_run(&cmd))
+        return 1;
 
-    cmd_append(&cmd, "cc","-E", "-P" XMACRO_SRC, "-o", XMACRO_ELF);
+    // cmd_append(&cmd, "cc", "-E", "-P" , XMACRO_SRC, "-o", XMACRO_ELF);
+    // if (!cmd_run(&cmd))
+    //     return 1;
+
+    cmd_append(&cmd, "cc", PTR_FUNCTION_APP_SRC, PTR_FUNCTION_APPFLASH_SRC, "-o", "app.elf");
+    if (!cmd_run(&cmd))
+        return 1;
 
     return 0;
 }
